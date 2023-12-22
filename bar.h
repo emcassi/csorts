61f1aba3134f12153c3882fdf6bdd7d1ffbe88f4
Alex Wayne <alex.wayne.dev@gmail.com>
2023-12-22 05:11:08 -0700

    SDL boilerplate & can draw bars

---
+++ bar.h
@@ -0,0 +1,15 @@
+#include <SDL2/SDL_render.h>
+typedef struct Bar {
+  int x;
+  int y;
+  int h;
+  int status;
+} Bar;
+
+enum BarStatus {
+  BAR_DEFAULT,
+};
+
+Bar* CreateBar(int x, int y, int h);
+void DrawBar(SDL_Renderer *renderer, Bar *bar);
+void DestroyBar(Bar *bar);
---
+++ colors.h
@@ -0,0 +1,5 @@
+#define GREEN 134, 186, 144, 255
+#define RED 223, 41, 53, 255
+#define WHITE 252, 234, 222, 255
+#define BG 65, 39, 34, 255
+
